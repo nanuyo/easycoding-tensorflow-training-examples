4c713e687997f6e668efa521df5eaedbeaad166a
HJ Park <hjparkg71@gmail.com>
2023-03-14 17:14:24 +0900

    chap5 update1

--- chap5/transfer_learning.py
+++ chap5/transfer_learning.py
@@ -2,6 +2,9 @@ import urllib.request
 from tensorflow.keras import layers
 from tensorflow.keras import Model
 from tensorflow.keras.applications.inception_v3 import InceptionV3
+import zipfile
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras.optimizers import RMSprop
 
 weights_url = "https://storage.googleapis.com/mledu-datasets/inception_v3_weights_tf_dim_ordering_tf_kernels_notop.h5"
 weights_file = "inception_v3.h5"
@@ -12,3 +15,64 @@ pre_trained_model = InceptionV3(input_shape=(150, 150, 3),
 pre_trained_model.load_weights(weights_file)
 
 pre_trained_model.summary()
+
+for layer in pre_trained_model.layers:
+    layer.trainable = False
+
+last_layer = pre_trained_model.get_layer('mixed7')
+print('마지막 층의 출력 크기: ', last_layer.output_shape)
+last_output = last_layer.output
+
+
+# 출력을 펼쳐서 1차원으로 만듭니다.
+x = layers.Flatten()(last_output)
+# 1,204개 은닉 유닛과 렐루 활성화 함수를 사용한 완전 연결 층을 추가합니다.
+x = layers.Dense(1024, activation='relu')(x)
+# 분류를 위해 시그모이드 함수를 사용하는 최종 층을 추가합니다.
+x = layers.Dense(1, activation='sigmoid')(x)
+
+model = Model(pre_trained_model.input, x)
+
+model.compile(optimizer=RMSprop(learning_rate=0.0001),
+              loss='binary_crossentropy',
+              metrics=['acc'])
+
+
+url = "https://storage.googleapis.com/learning-datasets/horse-or-human.zip"
+file_name = "horse-or-human.zip"
+training_dir = 'horse-or-human/training/'
+urllib.request.urlretrieve(url, file_name)
+zip_ref = zipfile.ZipFile(file_name, 'r')
+zip_ref.extractall(training_dir)
+zip_ref.close()
+
+validation_url = "https://storage.googleapis.com/learning-datasets/validation-horse-or-human.zip"
+validation_file_name = "validation-horse-or-human.zip"
+validation_dir = 'horse-or-human/validation/'
+urllib.request.urlretrieve(validation_url, validation_file_name)
+zip_ref = zipfile.ZipFile(validation_file_name, 'r')
+zip_ref.extractall(validation_dir)
+zip_ref.close()
+
+train_datagen = ImageDataGenerator(rescale=1/255)
+
+train_generator = train_datagen.flow_from_directory(
+    training_dir,
+    batch_size=20,
+    class_mode='binary',
+    target_size=(150, 150)
+)
+
+validation_datagen = ImageDataGenerator(rescale=1/255)
+
+validation_generator = validation_datagen.flow_from_directory(
+    validation_dir,
+    target_size=(150, 150),
+    class_mode='binary'
+)
+
+model.fit(train_generator,
+          epochs=40,
+          validation_data=validation_generator)
+
+model.save('horse_human_transfer_learning_model')
