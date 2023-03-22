2240361d78a19988eba3a102266d22d252b5b619
HJ Park <hjparkg71@gmail.com>
2023-03-22 11:19:48 +0900

    chap5 add cat dog

--- chap5/cat_dog_transfer_learning.py
+++ chap5/cat_dog_transfer_learning.py
@@ -96,27 +96,18 @@ pre_trained_model = InceptionV3(input_shape=(150, 150, 3),
                                 weights=None)
 
 
-pre_trained_model.summary()
 
 pre_trained_model.load_weights(weights_file)
-
-print(pre_trained_model.layers)
 
 for layer in pre_trained_model.layers:
     layer.trainable = False
 
+pre_trained_model.summary()
 
 last_layer = pre_trained_model.get_layer('mixed7')
-print('last layer output shape: ', last_layer.output_shape)
-last_output = last_layer.output
 
-# Flatten the output layer to 1 dimension
-x = layers.Flatten()(last_output)
-# Add a fully connected layer with 1,024 hidden units and ReLU activation
+x = layers.Flatten()(last_layer.output)
 x = layers.Dense(1024, activation='relu')(x)
-# Add a dropout rate of 0.2
-#x = layers.Dropout(0.2)(x)
-# Add a final sigmoid layer for classification
 x = layers.Dense(1, activation='sigmoid')(x)
 
 model = Model(pre_trained_model.input, x)
--- chap5/weights_download.py
+++ chap5/weights_download.py
@@ -14,31 +14,34 @@ pre_trained_model = InceptionV3(input_shape=(150, 150, 3),
                                 weights=None)
 
 
-pre_trained_model.summary()
+# pre_trained_model.summary()
 
 pre_trained_model.load_weights(weights_file)
 
 #print(pre_trained_model.layers)
 
 for layer in pre_trained_model.layers:
-    print(layer)
-    print(layer.trainable)
+    # print(layer)
+    # print(layer.trainable)
     layer.trainable = False
 
 
 last_layer = pre_trained_model.get_layer('mixed7')
 print('last layer output shape: ', last_layer.output_shape)
-last_output = last_layer.output
-print(last_output)
+
+print(last_layer.output)
 # Flatten the output layer to 1 dimension
-x = layers.Flatten()(last_output)
+x = layers.Flatten()(last_layer.output)
+print(x)
 # Add a fully connected layer with 1,024 hidden units and ReLU activation
 x = layers.Dense(1024, activation='relu')(x)
+print(x)
 # Add a dropout rate of 0.2
 #x = layers.Dropout(0.2)(x)
 # Add a final sigmoid layer for classification
 x = layers.Dense(1, activation='sigmoid')(x)
+print(x)
 print(pre_trained_model.input)
 model = Model(pre_trained_model.input, x)
 
-model.summary()
+# model.summary()
